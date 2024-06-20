86b11e5d8cc8550a1ebbbf7cf69039f3cdccad04
Rameshwar mishra <122230210+Rameshwar1812@users.noreply.github.com>
2024-06-20 21:46:32 +0530

    Add files via upload

---
+++ Lecture 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//Declare 2 variable and assign then values of 3,5 and then add them
+	int a;
+	int b;
+	
+	a=3;
+	b=5;
+	
+	int c;
+	c=a+b;
+	
+	cout<<c<<endl;
+	// c is not given
+	cout<<a+b;
+	cout<<"a+b";
+}
