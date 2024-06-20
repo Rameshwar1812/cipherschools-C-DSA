de1db4f2a342f7cf09522279a3ea1a1c16c2dc43
Rameshwar mishra <122230210+Rameshwar1812@users.noreply.github.com>
2024-06-20 22:17:27 +0530

    Add files via upload

---
+++ Lecture 7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+//	int x;
+	//variable na,e x of type int......
+	
+	//Arrray can be used to store multiple values in a single variable name
+	
+//	int a[5];  //element strating from 0-5
+	int a,b,c,d,e;
+	cout<<"Give 5 integers as input "<<endl;
+	cin>>a>>b>>c>>d>>e;
+	cout<<"The integers in reverse order are: "<<e<<d<<c<<b<<a;
+
+return 0;
+	
+} 
